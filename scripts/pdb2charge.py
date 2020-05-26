5954e184f0ec588430da481a2e440567973122d5
Hannah Bruce Macdonald <hannah.brucemacdonald@choderalab.org>
2020-05-26 11:14:58 +0100

    adding Soohaeng's scripts

---
+++ scripts/pdb2charge.py
@@ -0,0 +1,44 @@
+from simtk.openmm.app import *
+from simtk.openmm import *
+from simtk.unit import elementary_charge
+from math import floor
+import numpy
+
+def pdb2charge (fname_pdb):
+    '''
+    read pdb file and forcefield files and then
+    return charges
+    ---
+    fname_pdb : string
+    fname_ff  : [string, string, ..]
+    return 
+    chg_list : numpy.array ([q1, q2, ...])
+    '''
+    pdb = PDBFile (fname_pdb)
+    ff  = ForceField ('amber14/protein.ff14SB.xml', 'amber14/tip3p.xml')
+    
+    sys = ff.createSystem (pdb.topology)
+
+
+    for i in range (sys.getNumForces()):
+        if isinstance(sys.getForce(i), NonbondedForce):
+            nonbonded = sys.getForce(i)
+            break
+
+    
+    chg_list = []
+
+    for i in range(nonbonded.getNumParticles()):
+        nb_i = nonbonded.getParticleParameters(i)
+        chg  = nb_i[0].value_in_unit(elementary_charge)
+        chg_list.append (chg)
+    
+    return numpy.array (chg_list)
+
+if __name__ == "__main__":
+    fname_pdb = 'old_solvent_nomol.pdb'
+  
+    chg_list = pdb2charge (fname_pdb)
+    
+    print (chg_list)
+    
